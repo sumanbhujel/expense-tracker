f0fd043cb64bd4438b932fda5c8f80536fb4648e
sumanbhujel <xnamus@outlook.com>
2020-07-07 11:20:05 +0545

    helper package is added

---
+++ app/src/main/java/com/agileproject/expense_tracker/helper/ApiError.java
@@ -0,0 +1,12 @@
+package com.agileproject.expense_tracker.helper;
+
+import com.agileproject.expense_tracker.models.Error;
+
+public class ApiError {
+
+    private Error error;
+
+    public Error getError() {
+        return error;
+    }
+}
---
+++ app/src/main/java/com/agileproject/expense_tracker/models/Error.java
@@ -0,0 +1,13 @@
+package com.agileproject.expense_tracker.models;
+
+public class Error {
+    private String field, message;
+
+    public String getField() {
+        return field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
