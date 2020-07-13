a3955ba824feb45655b3eee25c3eda1e9d5172e8
sumanbhujel <xnamus@outlook.com>
2020-07-13 14:16:51 +0545

    addinccategory fragment is added

---
+++ app/src/main/java/com/agileproject/expense_tracker/fragments/AddIncCategory.java
@@ -0,0 +1,33 @@
+package com.agileproject.expense_tracker.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.agileproject.expense_tracker_app.R;
+
+public class AddIncCategory extends Fragment {
+
+
+    public AddIncCategory() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_inc_category, container, false);
+    }
+}
