31731d2a28560c8c68b9617a4701fb9172cc10e4
sumanbhujel <xnamus@outlook.com>
2020-07-27 09:35:14 +0545

    CategoryActivity is added

---
+++ app/src/main/java/com/agileproject/expense_tracker/activities/CategoryActivity.java
@@ -0,0 +1,17 @@
+package com.agileproject.expense_tracker.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.agileproject.expense_tracker.R;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+        getSupportActionBar().setTitle("Categories");
+    }
+}
