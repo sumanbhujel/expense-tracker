c698acd1220844193704fa0f304486f794421cc7
sumanbhujel <xnamus@outlook.com>
2020-07-28 12:22:34 +0545

    allcategoriesfragment is added

--- app/src/main/java/com/agileproject/expense_tracker/activities/CategoryActivity.java
+++ app/src/main/java/com/agileproject/expense_tracker/activities/CategoryActivity.java
@@ -2,11 +2,17 @@ package com.agileproject.expense_tracker.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.agileproject.expense_tracker.R;
 
-public class CategoryActivity extends AppCompatActivity {
+public class CategoryActivity extends AppCompatActivity  {
+
+
+    private String categoryId = "";
 
 
     @Override
@@ -14,5 +20,15 @@ public class CategoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
         getSupportActionBar().setTitle("Categories");
+        Button btnAddNewCat = findViewById(R.id.btn_add_new_cat);
+        btnAddNewCat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i5 = new Intent(CategoryActivity.this, AddCategoryActivity.class);
+                startActivity(i5);
+            }
+        });
+
+        //loadFragment(AllCategoriesFragment.newInstance("Categories"));
     }
 }
