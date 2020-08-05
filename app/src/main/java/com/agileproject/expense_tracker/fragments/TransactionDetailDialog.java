3fb4b8b1e04f891871859d1616c6b35041f4473e
sumanbhujel <xnamus@outlook.com>
2020-08-05 14:18:55 +0545

    detaildialog design is done

--- app/src/main/java/com/agileproject/expense_tracker/fragments/TransactionDetailDialog.java
+++ app/src/main/java/com/agileproject/expense_tracker/fragments/TransactionDetailDialog.java
@@ -1,4 +1,34 @@
 package com.agileproject.expense_tracker.fragments;
 
-public class TransactionDetailDialog {
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import com.agileproject.expense_tracker.helper.ConfirmationDialog;
+import com.google.android.material.textfield.TextInputLayout;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class TransactionDetailDialog extends AppCompatDialogFragment implements ConfirmationDialog.ConfirmationDialogListener {
+
+    private TextView tvCategory, tvType;
+    private TextInputLayout tvAmount, tvDate, tvNote;
+    private ImageView transactionImage;
+    private String creator, transactionId = "";
+    private String category = "";
+    private CircleImageView categoryIcon;
+    private ConfirmationDialog deleteTransDialog;
+    //private TransactionR transaction;
+    //private TransactionBLL transactionBLL;
+
+    @Override
+    public void onSure() {
+
+    }
+
+    @Override
+    public void onCancel() {
+
+    }
 }
