3a1c3887b6f38119b6c4d15d66c3bbde4a731ff5
YogendraSherpunja <yogendramagar15@gmail.com>
2020-08-06 15:31:46 +0545

    Update TDD test for transaction

--- app/src/test/java/com/agileproject/expense_tracker/TransactionUnitTest.java
+++ app/src/test/java/com/agileproject/expense_tracker/TransactionUnitTest.java
@@ -8,7 +8,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertNull;
+import static junit.framework.TestCase.assertTrue;
 
 public class TransactionUnitTest {
 
@@ -90,6 +92,26 @@ public class TransactionUnitTest {
         TransactionResponse transactionResponse = transactionBLL.getTransactions("5d879975de6f522844aa111e");
         assertEquals(2, transactionResponse.getMyTransactions().size());
     }
+
+
+    //test for update transaction
+    @Test
+    public void testK_validId_shouldUpdateTransaction() {
+        String transactionId = "5dad0dbe2194501f788ca877";
+        Transaction newTransaction = new Transaction("Test Transaction Update", "Expense", "5da6c2393cc60c2cb021cdcb", "2019-10-08", "5dad074c8087bd0f0c88829a", 1500);
+        TransactionResponse updateTransactionResponse = transactionBLL.updateTransaction(transactionId, newTransaction);
+        assertEquals("Test Transaction Update", updateTransactionResponse.getTransaction().getNote());
+    }
+
+    @Test
+    public void testL_invalidId_shouldReturnNull() {
+        String transactionId = "5d879975de6f522844aa111e";
+        Transaction newTransaction = new Transaction("Test Transaction Update", "Expense", "5da6c2393cc60c2cb021cdcb", "2019-10-08", "5dad074c8087bd0f0c88829a", 1500);
+        TransactionResponse updateTransactionResponse = transactionBLL.updateTransaction(transactionId, newTransaction);
+        assertNull(updateTransactionResponse);
+    }
+
+
 
 
 }
