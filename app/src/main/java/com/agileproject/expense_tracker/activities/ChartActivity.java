d3e49c2cb3fb431e79b6702612cc0e9873dc75fd
sumanbhujel <xnamus@outlook.com>
2020-08-10 16:34:15 +0545

    Piechart is added

--- app/src/main/java/com/agileproject/expense_tracker/activities/ChartActivity.java
+++ app/src/main/java/com/agileproject/expense_tracker/activities/ChartActivity.java
@@ -2,15 +2,96 @@ package com.agileproject.expense_tracker.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import com.agileproject.expense_tracker.R;
+import com.agileproject.expense_tracker.bll.TransactionBLL;
+import com.agileproject.expense_tracker.helper.UserSession;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChartActivity extends AppCompatActivity {
+
+    private PieChart pieChart;
+    private UserSession userSession;
+    private TransactionBLL transactionBLL;
+    private HashMap<String, Double> myTransactions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
+
+        userSession = new UserSession(this);
+        transactionBLL = new TransactionBLL();
+        myTransactions = new HashMap<>();
+
+        pieChart = findViewById(R.id.transactions_piechart);
+    }
+
+    public void drawChart() {
+
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5, 10, 5, 5);
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
+
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.setTransparentCircleColor(Color.WHITE);
+        pieChart.setTransparentCircleAlpha(110);
+
+        pieChart.setHoleRadius(58f);
+        pieChart.setTransparentCircleRadius(61f);
+        pieChart.setDrawCenterText(true);
+        pieChart.setRotationAngle(0);
+        pieChart.setRotationEnabled(true);
+        pieChart.setHighlightPerTapEnabled(true);
+
+        pieChart.animateY(1200, Easing.EaseInOutQuad);
+
+        Legend l = pieChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setTextSize(14f);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(0f);
+        l.setYOffset(0f);
+
+        pieChart.setEntryLabelColor(Color.WHITE);
+        pieChart.setEntryLabelTextSize(12f);
+
+        ArrayList<PieEntry> pieChartArrayList = new ArrayList<>();
+        for (Map.Entry<String, Double> entry : myTransactions.entrySet()) {
+            pieChartArrayList.add(new PieEntry(entry.getValue().floatValue(), entry.getKey()+""));
+        }
+
+        PieDataSet pieDataSet = new PieDataSet(pieChartArrayList, "My Transactions");
+        pieDataSet.setSliceSpace(3f);
+        pieDataSet.setSelectionShift(5f);
+        pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+
+        PieData pieData = new PieData(pieDataSet);
+        pieData.setValueFormatter(new PercentFormatter(pieChart));
+        pieData.setValueTextSize(15f);
+        pieData.setValueTypeface(Typeface.DEFAULT_BOLD);
+        pieData.setValueTextColor(Color.WHITE);
+
+        pieChart.setData(pieData);
+        pieChart.invalidate();
     }
 }
